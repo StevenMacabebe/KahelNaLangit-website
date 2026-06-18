59085e8e2c8f558233631767f8f90902cd30efc2
darleneavenir <darleneavenir@gmail.com>
2026-06-18 23:11:04 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background: #f8f9fa;
+            background: linear-gradient(135deg, #ffb437, #ffa003, #ff6c27, #d88531);
             height: 100vh;
             display: flex;
             align-items: center;
