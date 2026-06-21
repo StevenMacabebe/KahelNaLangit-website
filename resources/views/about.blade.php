a1c1f310bdc96f791b03167461c1d2187d8bbfe1
darleneavenir <darleneavenir@gmail.com>
2026-06-22 03:38:35 +0800

    script

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -292,24 +292,22 @@
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"> 
-        
-        /* SDG MODAL LOGIC */
- 
-        function openSdg(img, title, desc) {
-        document.getElementById('sdgImg').src = img;
-        document.getElementById('sdgTitle').innerHTML = title;
-        document.getElementById('sdgDesc').innerHTML = desc;
-        document.getElementById('sdgOverlay').classList.add('show');
-    }
-    function closeSdg() {
-        document.getElementById('sdgOverlay').classList.remove('show');
-    }
-    document.getElementById('sdgOverlay').addEventListener('click', function(e) {
-        if (e.target === this) closeSdg();
-    });
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-    </script>   
+    <script>
+        function openSdg(img, title, desc) {
+            document.getElementById('sdgImg').src = img;
+            document.getElementById('sdgTitle').innerHTML = title;
+            document.getElementById('sdgDesc').innerHTML = desc;
+            document.getElementById('sdgOverlay').classList.add('show');
+        }
+        function closeSdg() {
+            document.getElementById('sdgOverlay').classList.remove('show');
+        }
+        document.getElementById('sdgOverlay').addEventListener('click', function(e) {
+            if (e.target === this) closeSdg();
+        });
+    </script> 
 
 </body>
 </html>
