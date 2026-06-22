b5487e71b9520beb6caababcb87a6140e0cff7f3
StevenMacabebe <sjomacabebe@mymail.mapua.edu.ph>
2026-06-22 22:47:19 +0800

    login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background: #c25328;  /* solid orange */
+           
+            background-image: url('{{ asset("public/images/authbg.png") }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
             height: 100vh;
             display: flex;
             align-items: center;
             justify-content: center;
             font-family: 'Poppins', sans-serif;
+            margin: 0;
         }
         .login-card {
             max-width: 400px;
@@ -29,7 +34,7 @@
             margin: 0 auto 20px;
         }
         .login-card h2 {
-            color: #365fa9;   /* blue heading */
+            color: #365fa9;
             text-align: center;
             font-weight: 700;
             margin-bottom: 25px;
