3228da88b32cf79e98218c9f0fa48ca152f1959d
StevenMacabebe <sjomacabebe@mymail.mapua.edu.ph>
2026-06-29 21:57:32 +0800

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home - Kahel na Langit</title>
+
+    <!-- ===== FAVICON (browser tab icon) ===== -->
+    <link rel="icon" href="{{ asset('public/images/logo.png') }}" type="image/png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Schoolbell&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
